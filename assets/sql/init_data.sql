cf3f2c4a0a3e370820bbaef8f82af263a3fda9d1
Fernando-Emanuel <fernando2004emanuel@gmail.com>
2021-11-18 17:24:51 +0000

    config

--- assets/sql/init_data.sql
+++ assets/sql/init_data.sql
@@ -1,7 +1,6 @@
 -- Código SQL com a inclusão dos registros iniciais do banco de dados
 
-INSERT INTO filmes(titulo) VALUES
+INSERT INTO filmes (titulo) VALUES
     ('Entre Facas e Segredos'),
-    ('O Hobbit'),
-    ('Guia do Mochileiro das Galáxias'),
-    ('Monty Python: Em Busca do Cálice Sagrado');
+    ('A Sociedade do Anel'),
+    ('O Hobbit');
